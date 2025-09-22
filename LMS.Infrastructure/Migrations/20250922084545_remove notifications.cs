79de91ff89e22f2fab6eaca68b3c158858e75c68
riwahoussari <houssaririwa@gmail.com>
2025-09-22 11:46:07 +0300

    final migrations

---
+++ LMS.Infrastructure/Migrations/20250922084545_remove notifications.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class removenotifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotificationRecipients",
+                schema: "lms");
+
+            migrationBuilder.DropTable(
+                name: "Notifications",
+                schema: "lms");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                schema: "lms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Link = table.Column<string>(type: "text", nullable: true),
+                    Message = table.Column<string>(type: "text", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NotificationRecipients",
+                schema: "lms",
+                columns: table => new
+                {
+                    NotificationId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RecipientId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Opened = table.Column<bool>(type: "boolean", nullable: false),
+                    OpenedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationRecipients", x => new { x.NotificationId, x.RecipientId });
+                    table.ForeignKey(
+                        name: "FK_NotificationRecipients_Notifications_NotificationId",
+                        column: x => x.NotificationId,
+                        principalSchema: "lms",
+                        principalTable: "Notifications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NotificationRecipients_Users_RecipientId",
+                        column: x => x.RecipientId,
+                        principalSchema: "auth",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationRecipients_RecipientId_CreatedAt",
+                schema: "lms",
+                table: "NotificationRecipients",
+                columns: new[] { "RecipientId", "CreatedAt" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationRecipients_RecipientId_Opened",
+                schema: "lms",
+                table: "NotificationRecipients",
+                columns: new[] { "RecipientId", "Opened" });
+        }
+    }
+}
